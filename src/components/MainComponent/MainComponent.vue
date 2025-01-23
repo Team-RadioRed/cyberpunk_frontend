e105607712215cf91b527fbed35073a9b9c30d88
Denis Yudenko <denis.yueo@gmail.com>
2025-01-24 00:08:46 +0300

    Create MainComponent.vue

---
+++ src/components/MainComponent/MainComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="main_container">
+    <guide-component/>
+    <article-component/>
+    <generator-component/>
+  </main>
+</template>
+
+<script>
+import GuideComponent from './GuideComponent/GuideComponent.vue';
+import ArticleComponent from './ArticleComponent/ArticleComponent.vue';
+import GeneratorComponent from './GeneratorComponent/GeneratorComponent.vue';
+
+export default {
+  name: 'MainComponent',
+  components: {
+    GuideComponent,
+    ArticleComponent,
+    GeneratorComponent
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.main_container {
+  margin-top: 60px;
+  padding: 0 46px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  justify-content: center;
+  gap: 66px;
+}
+</style>
