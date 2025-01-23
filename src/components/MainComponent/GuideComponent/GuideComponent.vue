423da86d8c40183a23b75832b2fdb86fb84366a9
Denis Yudenko <denis.yueo@gmail.com>
2025-01-24 00:08:36 +0300

    Create GuideComponent.vue

---
+++ src/components/MainComponent/GuideComponent/GuideComponent.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="guide_container">
+    <div class="guide_wrapper">
+      <div class="title_wrapper">
+        <div class="title_icon">
+          <img
+            src="@/assets/svg/icon_title.svg"
+            alt="icon">
+        </div>
+        <h2 class="guide_title">
+          Популярные справочники
+        </h2>
+      </div>
+      <ul class="guide_elements">
+        <li class="first_element guide_element">
+          <h3 class="guide_element_title">
+            &#91; ОРУЖИЕ &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; Импланты &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; Нетранинг &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; Расходники &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; Роли &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; Транспорт &#93;
+          </h3>
+        </li>
+        <li class="guide_element">
+          <div class="guide_element_back"/>
+          <h3 class="guide_element_title">
+            &#91; NPC &#93;
+          </h3>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'GuideComponent',
+  components: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.guide_container {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  gap: 25px;
+  align-self: flex-end;
+  .guide_wrapper {
+    width: 100%;
+    max-width: 646px;
+    display: flex;
+    flex-direction: column;
+    gap: 25px;
+    .title_wrapper {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      .title_icon {
+        display: flex;
+        align-items: center;
+        img {
+          height: 16px;
+          display: inline-block;
+        }
+      }
+      .guide_title {
+        text-align: start;
+        text-transform: uppercase;
+        font-size: 26px;
+        font-weight: 500;
+      }
+    }
+    .guide_elements {
+      list-style: none;
+      display: grid;
+      grid-template-columns: repeat(2, 311px);
+      grid-template-rows: repeat(4, 145px);
+      gap: 21px;
+      box-sizing: border-box;
+      .first_element {
+        grid-row: span 2;
+      }
+      .guide_element {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        transition: 0.8s;
+        background-repeat: no-repeat;
+        cursor: pointer;
+        .guide_element_title {
+          margin-bottom: 20px;
+          margin-left: 20px;
+          color: $color-red;
+          font-size: 24px;
+          font-weight: 400;
+          transition: 0.5s;
+          text-transform: uppercase;
+        }
+        &:hover {
+          transition: 0.8s;
+          .guide_element_title {
+            margin-left: 40px;
+          }
+        }
+      }
+      li:nth-child(1) {
+        background: url('@/assets/svg/back_weapon.svg');
+      }
+      li:nth-child(2) {
+        background: url('@/assets/svg/back_implants.svg');
+      }
+      li:nth-child(3) {
+        background: url('@/assets/svg/back_netraning.svg');
+      }
+      li:nth-child(4) {
+        background: url('@/assets/svg/back_material.svg');
+      }
+      li:nth-child(5) {
+        background: url('@/assets/svg/back_role.svg');
+      }
+      li:nth-child(6) {
+        background: url('@/assets/svg/back_transport.svg');
+      }
+      li:nth-child(7) {
+        background: url('@/assets/svg/back_npc.svg');
+      }
+    }
+  }
+}
+</style>
