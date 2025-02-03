92bb17b178d8a8261997b638f7d9ca7c2ede48fe
Denis Yudenko <denis.yueo@gmail.com>
2025-02-04 01:16:39 +0300

    Create InProgressComponent.vue

---
+++ src/components/Other/InProgressComponent/InProgressComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="element_inprogress">
+    В РАЗРАБОТКЕ
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InProgressComponent',
+  props: {
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.element_inprogress {
+  position: absolute;
+  top: -4px;
+  left: -4px;
+  width: 135px;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: linear-gradient(-45deg, transparent 7px, $color-light-blue 0);
+  color: $color-dark-blue;
+  font-size: 16px;
+  font-weight: 500;
+}
+</style>
